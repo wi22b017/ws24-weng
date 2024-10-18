aa918223458199cff0b5d1ae1b3b48b135bb9252
wi22b017 <wi22b017@technikum-wien.at>
2024-10-18 22:32:13 +0200

    first commit

---
+++ bweng/src/test/java/at/fhtw/bweng/BwengApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.bweng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BwengApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
