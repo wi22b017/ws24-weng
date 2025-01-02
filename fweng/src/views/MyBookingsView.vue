5b2013741ebb516b3326109bb576a8742456fef3
xss9149 <132003432+xss9149@users.noreply.github.com>
2025-01-02 11:29:13 +0100

    created MyBookingsView.vue

--- fweng/src/views/MyBookingsView.vue
+++ fweng/src/views/MyBookingsView.vue
@@ -1,9 +1,34 @@
 <template>
-  <h1 class="mt-5">My Bookings</h1>
-  <h2 class="mt-3">Todo for User</h2>
+  <div class="container mt-4">
+  <h1 class="mt-5 mb-3">My Bookings</h1>
+  <MyBookingsTemplate
+      v-if="bookings.length > 0"
+      :bookings="bookings"
+      @cancel-booking="handleCancelBooking"
+
+  />
+    <div v-if="bookings.length === 0">No bookings have been made.</div>
+  </div>
 </template>
 
 <script setup>
+import {useUserStore} from "@/store/user";
+import MyBookingsTemplate from "@/components/template/MyBookingsTemplate.vue";
+
+const userStore = useUserStore();
+
+const bookings = userStore.bookings
+
+async function handleCancelBooking(bookingId) {
+  const result = await userStore.cancelBooking(bookingId);
+
+  if (result.success) {
+    alert("Booking cancelled successfully.");
+  } else {
+    alert(`Error: ${result.message}`);
+  }
+
+}
 
 </script>
 
